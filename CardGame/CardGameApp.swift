6530bebb507bd6ac2032fe2725ec7d34fed69abd
Igor Lima Ponce <Igorlima0748@gmail.com>
2024-05-09 20:30:12 -0300

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by igor Lima Ponce on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardGame
+//
+//  Created by igor Lima Ponce on 09/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

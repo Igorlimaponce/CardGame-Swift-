c1b7e74ef494003fc1852a7c9957333d11d8b77b
Igor Lima Ponce <Igorlima0748@gmail.com>
2024-05-09 20:54:09 -0300

    Add HStack com um teste

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
+        Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
             VStack {
                 Image(systemName: "globe")
                     .imageScale(.large)
@@ -17,6 +19,14 @@ struct ContentView: View {
                 Text("Hello, world!")
                 Text("Sejam bem vindos ao meu projeto!")
                     .padding()
+                
+                HStack{
+                    Image(systemName: "square.and.arrow.up")
+                        .imageScale(.small)
+                        .foregroundStyle(Color(.black))
+                        
+                    Text("Download")
+                }
             }
             .padding()
         }
